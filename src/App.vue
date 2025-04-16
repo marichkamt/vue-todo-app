ae055a5c3dada07eb9e0712cf3daf5c659c07669
marichkamt <emilycry1999@gmail.com>
2025-04-16 14:57:56 +0200

    todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,149 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, onMounted, ref, TransitionGroup, watch } from "vue";
+import StatusFilter from "./components/StatusFilter.vue";
+import TodoItem from "./components/TodoItem.vue";
+import * as todoApi from "./api/todos";
+import Message from "./components/Message.vue";
+
+const title = ref("");
+const status = ref("all");
+const todos = ref([]);
+const errorMessage = ref(null);
+
+onMounted(async () => {
+  try {
+    todos.value = await todoApi.getTodos();
+  } catch (error) {
+    errorMessage.value.show("Unable to load todos");
+  }
+});
+
+const activeTodos = computed(() =>
+  todos.value.filter((todo) => !todo.completed)
+);
+
+const visibleTodos = computed(() => {
+  if (status.value === "active") {
+    return activeTodos.value;
+  }
+
+  if (status.value === "completed") {
+    return todos.value.filter((todo) => todo.completed);
+  }
+
+  return todos.value;
+});
+
+const addTodo = async () => {
+  if (!title.value) {
+    errorMessage.value.show("Title should not be empty");
+    return;
+  }
+
+  try {
+    const newTodo = await todoApi.createTodo(title.value);
+
+    todos.value.push(newTodo);
+    title.value = "";
+  } catch (error) {
+    errorMessage.value.show("Unable to add a todo");
+  }
+};
+
+const updateTodo = async ({ id, title, completed }) => {
+  try {
+    const updatedTodo = await todoApi.updateTodo({ id, title, completed });
+    const currentTodo = todos.value.find((todo) => todo.id === id);
+
+    Object.assign(currentTodo, updatedTodo);
+  } catch (error) {
+    errorMessage.value.show("Unable to update a todo");
+  }
+};
+
+const deleteTodo = async (todoId) => {
+  try {
+    await todoApi.deleteTodo(todoId);
+    todos.value = todos.value.filter((todo) => todoId !== todo.id);
+  } catch (error) {
+    errorMessage.value.show("Unable to delete a todo");
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="todoapp">
+    <h1 class="todoapp__title">todos {{ todos.length }}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="todoapp__content">
+      <header class="todoapp__header">
+        <!-- this button should have `active` class only if all todos are completed -->
+        <button
+          v-if="todos.length > 0"
+          class="todoapp__toggle-all"
+          :class="{ active: activeTodos.length === 0 }"
+        ></button>
+
+        <form @submit.prevent="addTodo">
+          <input
+            class="todoapp__new-todo"
+            placeholder="What needs to be done?"
+            v-model="title"
+          />
+        </form>
+      </header>
+
+      <TransitionGroup
+        tag="section"
+        name="todolist"
+        class="todoapp__main"
+        v-if="todos.length > 0"
+      >
+        <TodoItem
+          v-for="todo of visibleTodos"
+          :key="todo.id"
+          :todo="todo"
+          @delete="deleteTodo(todo.id)"
+          @update="updateTodo($event)"
+        />
+      </TransitionGroup>
+
+      <footer class="todoapp__footer" v-if="todos.length > 0">
+        <span class="todo-count">{{ activeTodos.length }} items left</span>
+
+        <StatusFilter v-model="status" />
+
+        <button
+          class="todoapp__clear-completed"
+          :disabled="todos.length === activeTodos.length"
+        >
+          Clear completed
+        </button>
+      </footer>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <Message class="is-danger" ref="errorMessage">
+      <template #header>
+        <p>Server Error</p>
+      </template>
+
+      <template #default="{ text }">
+        <p>{{ text }}</p>
+      </template>
+    </Message>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.todolist-enter-active,
+.todolist-leave-active {
+  max-height: 60px;
+  transition: all 0.5s ease;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.todolist-enter-from,
+.todolist-leave-to {
+  opacity: 0;
+  max-height: 0;
+  transform: scaleY(0);
 }
 </style>
